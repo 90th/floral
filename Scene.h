6bb99c236700a2685684fb765e2a1ccf71de36a7
robert. <ninetieth@riseup.net>
2024-04-24 19:21:05 -0400

    updated gitignore

--- Scene.h
+++ Scene.h
@@ -1,11 +1,8 @@
 #ifndef SCENE_H
 #define SCENE_H
-
-// Abstract base class for scene rendering
 class Scene {
 public:
-	// Render method to be implemented by derived classes
 	virtual void Render() = 0;
 };
 
-#endif // SCENE_H
+#endif
