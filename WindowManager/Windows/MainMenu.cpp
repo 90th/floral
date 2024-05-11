8f79d3022ed397c7815816c1233caee43a1e6326
robert. <ninetieth@riseup.net>
2024-05-11 18:01:04 -0400

    [⚡] MainMenu Visuals

--- WindowManager/Windows/MainMenu.cpp
+++ WindowManager/Windows/MainMenu.cpp
@@ -5,19 +5,26 @@
 #include "SplashScreen.h"
 
 LPCSTR MainMenu::lpWindowName = "Main menu";
-ImVec2 MainMenu::vWindowSize = { 350, 450 };
 ImGuiWindowFlags MainMenu::WindowFlags = 0;
 
 void MainMenu::Render() {
-	ImGui::SetNextWindowSize(vWindowSize, ImGuiCond_Once);
+	ImGui::SetNextWindowSizeConstraints(ImVec2(350, 450), ImVec2(FLT_MAX, FLT_MAX)); // Set minimum window size
 	ImGui::SetNextWindowBgAlpha(1.0f);
 	ImGui::Begin(lpWindowName, &Drawing::bDraw, WindowFlags);
 	{
 		ImGui::Text("Username: %s", GlobalData::GetInstance()->getUserData().username.c_str());
-		ImGui::BeginChild("child", ImVec2(200, 200), true); {
-			ImGui::Text("This is a child.");
-			ImGui::EndChild();
+
+		ImGui::BeginChild("child1", ImVec2(0, 200), true, ImGuiWindowFlags_AlwaysAutoResize);
+		{
+			ImGui::Text("Child 1");
 		}
+		ImGui::EndChild();
+
+		ImGui::BeginChild("child2", ImVec2(0, 200), true, ImGuiWindowFlags_AlwaysAutoResize);
+		{
+			ImGui::Text("Child 2");
+		}
+		ImGui::EndChild();
 	}
 	ImGui::End();
 }
